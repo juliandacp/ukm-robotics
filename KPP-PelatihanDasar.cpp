fd96b45b932b304a88d673039522c094ea9e11e8
Julian <juliandacaesarprakoso@gmail.com>
2025-11-04 12:50:07 +0700

    first commit

---
+++ KPP-PelatihanDasar.cpp
@@ -0,0 +1,159 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int maks_N = 205;
+const int maks_M = 2005;
+const int maks_energi = 1000;
+const int INF = 1e9;
+//Saya menambahkan variabel baru, dimana ketika berada di rest point menghabiskan energi sebanyak 15
+//Ini saya lakukan agar output dapat sesuai dengan test case
+const int konsumsi_rest = 15;
+
+struct Edge{
+    char u, v;
+    int w, o;
+    int energi_dasar;
+};
+
+struct State{
+    char node;
+    int energi;
+    int waktu;
+    int total_energi;
+    bool jam_ganjil;
+    vector<char> path;
+    bool operator>(const State& other) const{
+        return total_energi > other.total_energi;
+    }
+};
+
+int main(){
+    int N, M;
+    cout << "N x M: ";
+    cin >> N >> M;
+    cout << "====PLAY====" << endl;
+    vector<Edge> edge(M);
+    map<char, vector<pair<char, int>>> graph;
+    for(int i = 0; i < M; i++){
+        cin >> edge[i].u >> edge[i].v >> edge[i].w >> edge[i].o;
+        edge[i].energi_dasar = edge[i].w + edge[i].o;
+        graph[edge[i].u].push_back({edge[i].v, i});
+        graph[edge[i].v].push_back({edge[i].u, i});
+    }
+    char start, target;
+    cin >> start >> target;
+    set<char> rest_point;
+    set<char> charging_station;
+    string temp;
+    cin >> temp;
+    if(temp != "-"){
+        rest_point.insert(temp[0]);
+        while(cin.peek() != '\n'){
+            cin >> temp;
+            rest_point.insert(temp[0]);
+        }
+    }
+    cin >> temp;
+    if(temp != "-"){
+        charging_station.insert(temp[0]);
+        while(cin.peek() != '\n'){
+            cin >> temp;
+            charging_station.insert(temp[0]);
+        }
+    }
+    char mechanic, electrical;
+    cin >> mechanic >> electrical;
+    int jam_mulai;
+    cin >> jam_mulai;
+    bool mulai_ganjil = (jam_mulai % 2 == 1);
+    map<pair<char, int>, int> best_energy;
+    priority_queue<State, vector<State>, greater<State>> pq;
+    State awal;
+    awal.node = start;
+    awal.energi = maks_energi;
+    awal.waktu = 0;
+    awal.total_energi = 0;
+    awal.jam_ganjil = mulai_ganjil;
+    awal.path = {start};
+    pq.push(awal);
+    best_energy[{start, maks_energi}] = 0;
+    State solusi_terbaik;
+    solusi_terbaik.total_energi = INF;
+    bool found = false;
+    while(!pq.empty()){
+        State sekarang = pq.top();
+        pq.pop();
+        if(sekarang.total_energi > best_energy[{sekarang.node, sekarang.energi}]){
+            continue;
+        }
+        if(sekarang.node == target){
+            if(sekarang.total_energi < solusi_terbaik.total_energi){
+                solusi_terbaik = sekarang;
+                found = true;
+            }
+            continue;
+        }
+        if(rest_point.count(sekarang.node) && sekarang.energi >= konsumsi_rest){
+            State state_tunggu = sekarang;
+            state_tunggu.waktu += 2;
+            state_tunggu.energi -= konsumsi_rest;
+            state_tunggu.total_energi += konsumsi_rest;
+            state_tunggu.jam_ganjil = !state_tunggu.jam_ganjil;
+            if(!best_energy.count({state_tunggu.node, state_tunggu.energi}) || 
+               state_tunggu.total_energi < best_energy[{state_tunggu.node, state_tunggu.energi}]){
+                best_energy[{state_tunggu.node, state_tunggu.energi}] = state_tunggu.total_energi;
+                pq.push(state_tunggu);
+            }
+        }
+        if(charging_station.count(sekarang.node)){
+            State state_isi = sekarang;
+            state_isi.energi = maks_energi;
+            if(!best_energy.count({state_isi.node, state_isi.energi}) || 
+               state_isi.total_energi < best_energy[{state_isi.node, state_isi.energi}]){
+                best_energy[{state_isi.node, state_isi.energi}] = state_isi.total_energi;
+                pq.push(state_isi);
+            }
+        }
+        for(auto& info_near : graph[sekarang.node]){
+            char node_berikut = info_near.first;
+            int idx_edge = info_near.second;
+            Edge& edge_sekarang = edge[idx_edge];
+            int energi_dibutuhkan;
+            if(sekarang.jam_ganjil) energi_dibutuhkan = round(edge_sekarang.energi_dasar * 1.3);
+            else energi_dibutuhkan = round(edge_sekarang.energi_dasar * 0.8);
+            if(energi_dibutuhkan > sekarang.energi) continue;
+            State state_berikut = sekarang;
+            state_berikut.node = node_berikut;
+            state_berikut.energi -= energi_dibutuhkan;
+            state_berikut.waktu += 2;
+            state_berikut.total_energi += energi_dibutuhkan;
+            state_berikut.jam_ganjil = !sekarang.jam_ganjil;
+            state_berikut.path.push_back(node_berikut);
+            if(!best_energy.count({state_berikut.node, state_berikut.energi}) || 
+               state_berikut.total_energi < best_energy[{state_berikut.node, state_berikut.energi}]){
+                best_energy[{state_berikut.node, state_berikut.energi}] = state_berikut.total_energi;
+                pq.push(state_berikut);
+            }
+        }
+    }
+    if(found){
+        cout << "Total energi minimum: " << solusi_terbaik.total_energi << endl;
+        cout << "Jalur: ";
+        for(int i = 0; i < solusi_terbaik.path.size(); i++){
+            if(i > 0) cout << " -> ";
+            cout << solusi_terbaik.path[i];
+        }
+        cout << endl;
+        cout << "Waktu tiba:" << endl;
+        int current_time = 0;
+        for(int i = 0; i < solusi_terbaik.path.size(); i++){
+            cout << solusi_terbaik.path[i] << " (menit " << current_time << ")" << endl;
+            if(i < solusi_terbaik.path.size() - 1){
+                current_time += 2;
+            }
+        }
+    } else {
+        cout << "Robot gagal dalam mencapai tujuan :(" << endl;
+    }
+    return 0;
+}
---
+++ KPP-RoboconWorkshop.cpp
@@ -0,0 +1,190 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Game{
+    public:
+    string nama_game;
+    string developer;
+    unsigned short tahun_rilis;
+    float rating;
+    float harga;
+    Game(){}
+    Game(string nama_gameInput, string developerInput, unsigned short tahun_rilisInput, float ratingInput, float hargaInput){
+        nama_game=nama_gameInput;
+        developer=developerInput;
+        tahun_rilis=tahun_rilisInput;
+        rating=ratingInput;
+        harga=hargaInput;
+    }
+    void inputDataUmum(){
+        cout << "\n=== INFORMASI GAME ===" << endl;
+        cout << "Nama Game: ";
+        getline(cin, nama_game);
+        cout << "Developer: ";
+        getline(cin, developer);
+        cout << "Tahun Rilis: ";
+        cin >> tahun_rilis;
+        cout << "Rating (0-10): ";
+        cin >> rating;
+        cout << "Harga: Rp";
+        cin >> harga;
+    }
+        void startAnalisis(){
+        cout << "\n=== ANALISIS GAME ===" << endl;
+    }
+};
+
+class RPG: public Game{
+    public:
+    unsigned int jumlah_karakter;
+    float alur_cerita;
+    unsigned int quest;
+    RPG(){}
+    RPG(string nama_gameInput, string developerInput, unsigned short tahun_rilisInput, float ratingInput, float hargaInput, unsigned int jumlah_karakterInput, float alur_ceritaInput, unsigned int questInput)
+        : Game(nama_gameInput, developerInput, tahun_rilisInput, ratingInput, hargaInput){
+            jumlah_karakter=jumlah_karakterInput;
+            alur_cerita=alur_ceritaInput;
+            quest=questInput;
+    }
+    void inputDataRPG(){
+        inputDataUmum();
+        cout << "Jumlah Karakter: ";
+        cin >> jumlah_karakter;
+        cout << "Alur Cerita (0-10): ";
+        cin >> alur_cerita;
+        cout << "Jumlah Quest: ";
+        cin >> quest;
+        cin.ignore();
+    }
+    void ujiKelayakan(){
+        startAnalisis();
+            if(rating>=7 && alur_cerita>=7.5 && quest>=25){
+                cout << "Status: LAYAK DIMAINKAN" << endl;
+                cout << "Alasan: " << endl;
+                cout << "- Rating sangat baik (" << rating << "/10)" << endl;
+                cout << "- Alur cerita luar biasa (" << alur_cerita << "/10)" << endl;
+                cout << "- " << quest << " quest tersedia" << endl;
+            }else{
+                cout << "Status: TIDAK LAYAK DIMAINKAN" << endl;
+                cout << "Alasan: " << endl;
+                if(rating<7) cout << "- Rating buruk (" << rating << "/10)" << endl;
+                if(alur_cerita<7.5) cout << "- Alur cerita kurang (" << alur_cerita << "/10)" << endl;
+                if(quest<25) cout << "- Quest yang tersedia hanya " << quest << " quest" << endl;
+            }
+        }
+};
+
+class FPS: public Game{
+    public:
+    unsigned int jumlah_map;
+    unsigned int jumlah_senjata;
+    unsigned int jumlah_player_aktif;
+    FPS(){}
+    FPS(string nama_gameInput, string developerInput, unsigned short tahun_rilisInput, float ratingInput, float hargaInput, unsigned int jumlah_mapInput, unsigned int jumlah_senjataInput, unsigned int jumlah_player_aktifInput)
+        : Game(nama_gameInput, developerInput, tahun_rilisInput, ratingInput, hargaInput){
+            jumlah_map=jumlah_mapInput;
+            jumlah_senjata=jumlah_senjataInput;
+            jumlah_player_aktif=jumlah_player_aktifInput;
+    }
+    void inputDataFPS(){
+        inputDataUmum();
+        cout << "Jumlah Map: ";
+        cin >> jumlah_map;
+        cout << "Jumlah Senjata: ";
+        cin >> jumlah_senjata;
+        cout << "Jumlah Player Aktif: ";
+        cin >> jumlah_player_aktif;
+        cin.ignore();
+    }
+    void ujiKelayakan(){
+        startAnalisis();
+        if(rating>=7 && jumlah_map>=5 && jumlah_player_aktif>=1000000){
+            cout << "Status: LAYAK DIMAINKAN" << endl;
+                cout << "Alasan: " << endl;
+                cout << "- Rating sangat baik (" << rating << "/10)" << endl;
+                cout << "- Memiliki " << jumlah_player_aktif << " pemain aktif" << endl;
+                cout << "- " << jumlah_map << " map tersedia" << endl;
+            }else{
+                cout << "Status: TIDAK LAYAK DIMAINKAN" << endl;
+                cout << "Alasan: " << endl;
+                if(rating<7) cout << "- Rating buruk (" << rating << "/10)" << endl;
+                if(jumlah_player_aktif<1000000) cout << "- Memiliki kurang dari 1 juta pemain aktif" << endl;
+                if(jumlah_map<5) cout << "- Map yang tersedia hanya " << jumlah_map << " map" << endl;
+        }
+    }
+};
+
+class Strategy: public Game{
+    public:
+    float kompleksitas;
+    float avg_lama_match;
+    float kesulitan;
+    Strategy(){}
+    Strategy(string nama_gameInput, string developerInput, unsigned short tahun_rilisInput, float ratingInput, float hargaInput, float kompleksitasInput, float avg_lama_matchInput, float kesulitanInput)
+        : Game(nama_gameInput, developerInput, tahun_rilisInput, ratingInput, hargaInput){
+            kompleksitas=kompleksitasInput;
+            avg_lama_match=avg_lama_matchInput;
+            kesulitan=kesulitanInput;
+    }
+    void inputDataStrategy(){
+        inputDataUmum();
+        cout << "Tingkat Kompleksitas (0-10): ";
+        cin >> kompleksitas;
+        cout << "Rata-rata Lama Match (Menit): ";
+        cin >> avg_lama_match;
+        cout << "Tingkat Kesulitan (0-10): ";
+        cin >> kesulitan;
+        cin.ignore();
+    }
+    void ujiKelayakan(){
+        startAnalisis();
+        if(rating>=7 && (kompleksitas>=5 || kesulitan>=5)){
+            cout << "Status: LAYAK DIMAINKAN" << endl;
+                cout << "Alasan: " << endl;
+                cout << "- Rating sangat baik (" << rating << "/10)" << endl;
+                cout << "- Kompleksitas permainan tinggi (" << kompleksitas << "/10)" << endl;
+                cout << "- Tingkat kesulitan game tinggi (" << kesulitan << "/10)" << endl;
+            }else{
+                cout << "Status: TIDAK LAYAK DIMAINKAN" << endl;
+                cout << "Alasan: " << endl;
+                if(rating<7) cout << "- Rating buruk (" << rating << "/10)" << endl;
+                if(kompleksitas<5) cout << "- Kompleksitas permainan kurang" << endl;
+                if(kesulitan<5) cout << "- Terlalu mudah" << endl;
+        }
+    }
+};
+
+int main(){
+    int opsi;
+    cout << "=== JENIS GAME ===" << endl;
+    cout << "1. RPG" << endl;
+    cout << "2. FPS" << endl;
+    cout << "3. Strategy" << endl;
+    cout << "Pilih Game (1-3): ";
+    cin >> opsi;  
+    cin.ignore();
+    switch(opsi){
+    case 1:{
+        RPG gameRPG;
+        gameRPG.inputDataRPG();
+        gameRPG.ujiKelayakan();
+        break;
+    }
+    case 2:{
+        FPS gameFPS;
+        gameFPS.inputDataFPS();
+        gameFPS.ujiKelayakan();
+        break;
+    }
+    case 3:{
+        Strategy gameStrategy;
+        gameStrategy.inputDataStrategy();
+        gameStrategy.ujiKelayakan();
+        break;
+    }
+    default:
+        cout << "Pilihan tidak valid!!!" << endl;
+        break;
+    }
+    return 0;
+}
